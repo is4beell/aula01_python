6262ef55f401ef3511d940082e596a0121dfa699
isabel.miranda@aluno.senac.rio <isabel.miranda@aluno.senac.rio>
2025-11-18 19:40:36 -0300

    Inicia o repositório da aula 01

---
+++ aula01/exemplo.py
@@ -0,0 +1,54 @@
+# nome = 'Ana'
+# idade = 25 
+# salario = 15000.0
+
+# print(f'O nome dela é {nome}, \nidade {idade} e o salario {salario}')
+
+# x = 5
+# x = '10'
+# print(x)
+
+# nome =  input('Digite o nome:   ')
+# print(f'Olá,  {nome}')
+
+# Cor = input('Qual a cor favorita?  ')
+# print(f'A sua cor é:  {Cor}')
+
+# n1 = float(input('Digite um valor:  '))
+# n2 = float(input('Digite outro valor:  '))
+# resultado = n1 * n2
+# print(f'O resultado foi:   {resultado}')
+
+# nome_usuario = input('Informe o nome:  ')
+# idade_usuario = input('Informe a idade:  ')
+# print(f'O nome é {nome_usuario} e sua idade é {idade_usuario}')
+
+# cidade_usuario = input('Informe sua cidade:  ')
+# bairro_usuario = input('Informe seu bairro:  ')
+# print(f'A cidade é {cidade_usuario} e seu bairro é {bairro_usuario}')
+
+# num1 = float(input ('Informe o número:  '))
+# num2 = float(input('Informe o segundo número:  '))
+# resultado = num1 * num2 
+# print(resultado)
+
+#Pergunte quantos anos eu tenho e ano de nascimento
+
+# idade = int(input ('Quantos anos você tem?  '))
+# print(f'Seu ano de nascimento é:  {2025 - idade}')
+
+# quanto_tem = float(input('Quanto você tem? R$  '))
+# ingresso = 30
+# print(f'Da para comprar {quanto_tem / ingresso} ingressos')
+
+# preco = float(input(f'Informe o preco:  '))
+# qtd = int(input('Informe a quantidade:  '))
+# total = preco * qtd
+# desconto = total * 1.0
+# valor_final = total - desconto
+# # print(f'Valor total {total}\nDesconto de 10% {desconto}\nValor a pagar:  {valor_final}')
+
+# salario = float(input(f'Informe o salario:  '))
+# acrescimo = salario * 1.15
+# print(f'Salário com aumento: R${acrescimo}')
+
